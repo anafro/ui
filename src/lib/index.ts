3a74c042ecdd00dfce16ecfd093c849d7bd5ec57
Anatoly <contact@anafro.ru>
2024-12-14 13:35:43 +1000

    chore: export components from library

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,23 @@
-// Reexport your entry components here
+import Box from "$lib/Box.svelte";
+import Button from "$lib/Button.svelte";
+import Checkbox from "$lib/Checkbox.svelte";
+import Field from "$lib/Field.svelte";
+import Header from "$lib/Header.svelte";
+import Logotype from "$lib/Logotype.svelte";
+import Radio from "$lib/Radio.svelte";
+import Text from "$lib/Text.svelte";
+import Tool from "$lib/Tool.svelte";
+import VectorImage from "$lib/VectorImage.svelte";
+
+export {
+    Box,
+    Button,
+    Checkbox,
+    Field,
+    Header,
+    Logotype,
+    Radio,
+    Text,
+    Tool,
+    VectorImage,
+};
